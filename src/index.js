f62eeb99a3025837458156564908a44ea26430b0
Oleg-Strong <superstrong0@gmail.com>
2023-06-20 01:50:23 +0300

    fix packade.json

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="tweets-test">
+    <BrowserRouter basename="/tweets-test">
       <App />
     </BrowserRouter>
   </React.StrictMode>
