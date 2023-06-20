c7a897abf6569a596192e6d9c15d9bdb33f85e0a
Oleg-Strong <superstrong0@gmail.com>
2023-06-20 03:21:26 +0300

    fix router

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="tweets-test/">
+    <BrowserRouter basename="tweets-test">
       <App />
     </BrowserRouter>
   </React.StrictMode>
